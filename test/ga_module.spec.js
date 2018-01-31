68cc1ecc89b04d7202b1a1d96de822ece764748b
gez <gerwyndaevidjones@gmail.com>
2018-01-31 21:17:43 +0000

    initial commit

---
+++ test/ga_module.spec.js
@@ -0,0 +1,8 @@
+var assert = require('assert');
+describe('ga module', () => {
+  describe('crossover', () => {
+    it('should swap a and b parts from each genome', () => {
+      assert.true(2 === 1);
+    });
+  });
+});
